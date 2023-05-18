24f7e80cdee5808d386bd4f8ddc09c9c1cd83748
sh1ft3r2 <joylynwambui817@gmail.com>
2023-05-18 12:40:43 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * puts - function that is used to print the output
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+		return (0);
+}
+
