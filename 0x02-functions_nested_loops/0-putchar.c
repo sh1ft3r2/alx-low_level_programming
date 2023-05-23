f8984cc223881e706da46f1a5faafd13012c6925
sh1ft3r2 <joylynwambui817@gmail.com>
2023-05-23 12:10:26 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * int main(void) - a program that prints _putchar, followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
