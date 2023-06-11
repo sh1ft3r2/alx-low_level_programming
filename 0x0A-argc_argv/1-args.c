02721fd694db13da1d79f8ce3f04af96917697d6
sh1ft3r2 <joylynwambui817@gmail.com>
2023-06-11 06:39:20 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: the first argument
+ * @argv: the second argument
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	(void)argv;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
