2251fb173583edbb9138202e4b141336085c0cd4
sh1ft3r2 <joylynwambui817@gmail.com>
2023-06-11 06:08:42 -0700

    Them arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: the first argument function
+ * @argv: the second  argument function
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+		return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Sweet babes Argc and Argv ;)
